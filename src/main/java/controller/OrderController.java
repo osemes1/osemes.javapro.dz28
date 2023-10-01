9a978e87909994f89a5e88cd471a13df99cbb90c
osemes1 <130790511+osemes1@users.noreply.github.com>
2023-10-01 09:11:17 +0300

    Commit dz28 2023-1001 09:11

--- src/main/java/controller/OrderController.java
+++ src/main/java/controller/OrderController.java
@@ -1,15 +1,19 @@
-
 package osemes.javapro.dz28.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import osemes.javapro.dz28.model.Order;
 import osemes.javapro.dz28.repository.OrderRepository;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import java.util.List;
 
-@RestController
-@RequestMapping("/orders")
+@Controller
+@Path("/orders")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class OrderController {
     private final OrderRepository orderRepository;
 
@@ -18,18 +22,19 @@ public class OrderController {
         this.orderRepository = orderRepository;
     }
 
-    @GetMapping("/{id}")
-    public Order getOrderById(@PathVariable Long id) {
+    @GET
+    @Path("/{id}")
+    public Order getOrderById(@PathParam("id") Long id) {
         return orderRepository.findById(id).orElse(null);
     }
 
-    @GetMapping
+    @GET
     public List<Order> getAllOrders() {
         return orderRepository.findAll();
     }
 
-    @PostMapping
-    public void addOrder(@RequestBody Order order) {
+    @POST
+    public void addOrder(Order order) {
         // Додаткова логіка для додавання замовлення
     }
 }
